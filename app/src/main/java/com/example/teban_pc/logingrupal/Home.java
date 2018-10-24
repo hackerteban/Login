f5ffa11cd66e43dec1b9ba19170e3cb584f72366
TEBAN-PC <estebanseyia@gmail.com>
2018-10-24 11:38:47 -0500

    Login

--- app/src/main/java/com/example/teban_pc/logingrupal/Home.java
+++ app/src/main/java/com/example/teban_pc/logingrupal/Home.java
@@ -1,13 +1,21 @@
 package com.example.teban_pc.logingrupal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    TextView txtUsuario;
+    TextView txtClave;
+    Button btnLogin;
+    Button btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,11 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        txtUsuario=(TextView) findViewById(R.id.txtUsuario);
+        txtClave=(TextView) findViewById(R.id.txtClave);
+        btnLogin=(Button) findViewById(R.id.btnLogin);
+        btnCancelar=(Button) findViewById(R.id.btnCancelar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +37,32 @@ public class Home extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void mostrarMensaje(View view){
+        String Usser="jtorresp";
+        String Pass="123456";
+
+        String usuario=txtUsuario.getText().toString();
+        String contrasena=txtClave.getText().toString();
+
+        switch(view.getId()){
+            case(R.id.btnLogin):
+                if(usuario.equals(Usser) && contrasena.equals(Pass)){
+                    Intent irVentana = new Intent(this, Home.class);
+                    irVentana.putExtra("nombre_01",Usser);
+                    irVentana.putExtra("nombre_01",Pass);
+                    startActivity(irVentana);
+                }else {
+                    Intent irVentana = new Intent(this, Error.class);
+                    startActivity(irVentana);
+                }
+                break;
+            case(R.id.btnCancelar):
+                txtUsuario.setText(null);
+                txtClave.setText(null);
+                break;
+        }
     }
 
 }
